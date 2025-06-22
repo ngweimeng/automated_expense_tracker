0f9128cb2033b8ebd4e54190c8c58f05a9417a3e
Ng Wei Meng <40542706+ngweimeng@users.noreply.github.com>
2025-06-22 12:30:10 +0000

    Revise layout configuration: change page layout from centered to wide for improved display

--- pages/01_About.py
+++ pages/01_About.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title="WeiMeng's Budget Tracker",
     page_icon="💰",
-    layout="centered",
+    layout="wide",
     initial_sidebar_state="expanded",
 )
 st.title("💰 WeiMeng's Budget Tracker")
