85f8a0e33b9573f5065442833fb33b699a53aa26
Ng Wei Meng <40542706+ngweimeng@users.noreply.github.com>
2025-06-22 08:19:07 +0000

    Fix case sensitivity in Streamlit page references in main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 pages = {
     "Navigate": [
-        st.page("pages/01_About.py", title="About the Project", icon="💡"),
-        st.page("pages/02_Dashboard.py", title="Dashboard", icon="📊"),
-        st.page("pages/03_Settings.py",  title="Settings",  icon="⚙️"),
+        st.Page("pages/01_About.py", title="About the Project", icon="💡"),
+        st.Page("pages/02_Dashboard.py", title="Dashboard", icon="📊"),
+        st.Page("pages/03_Settings.py",  title="Settings",  icon="⚙️"),
     ]
 }
 
